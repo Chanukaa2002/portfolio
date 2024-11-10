a4a0bcdecaf5061c09afd0065f5df94d66796dde
Chanukaa2002 <mchanuka72@gmail.com>
2024-11-10 13:20:22 +0530

    add some projects

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -3,7 +3,7 @@ import dp from "../assets/dp.jpg";
 
 function ProfilePicture() {
   return (
-    <div className="left-0 m-4  w-72 h-72 top-16">
+    <div className="left-0 m-4 w-72 h-72 top-16">
       {" "}
       {/* Use absolute positioning */}
       <img src={dp} className="border-4 border-green-400 rounded-full" />
